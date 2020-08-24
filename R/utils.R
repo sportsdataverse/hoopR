c6f934860cb7ec63f7406c07bb8a6637543ad044
saiemgilani <saiem.gilani@gmail.com>
2020-08-23 20:41:17 -0400

    Update website

--- R/utils.R
+++ R/utils.R
@@ -103,4 +103,22 @@ clean_team_names_NCAA_merge <- function(df){
 }
 
 
+#' Utilities and Helpers for package
+#' @keywords Internal
+#' @importFrom attempt "stop_if_not"
+#' @importFrom curl "has_internet"
+#'
+check_internet <- function(){
+  stop_if_not(.x = has_internet(), msg = "Please check your internet connexion")
+}
+
+#'
+#' @importFrom httr "status_code"
+#'
+
+check_status <- function(res){
+  stop_if_not(.x = status_code(res),
+              .p = ~ .x == 200,
+              msg = "The API returned an error")
+}
 
