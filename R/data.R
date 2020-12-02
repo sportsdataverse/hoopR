d6f5a1f6f5db0cb05cde9469b052b4d21f0de368
Saiem Gilani <saiem.gilani@gmail.com>
2020-12-02 17:22:07 -0500

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Teams Dictionary
+#' Team link reference lookup for the package
+#' @format A data frame with 357 rows and 6 variables:\
+#' \describe{
+#' \item{\code{Team}}{character.}
+#' \item{\code{Team.link}}{character.}
+#' \item{\code{team.link.ref}}{character.}
+#' \item{\code{Conf}}{character.}
+#' \item{\code{Conf.link}}{character.}
+#' \item{\code{conf.link.ref}}{character.}
+#' }
+"teams_links"
+
+
+
+
+
+
+
+
+
+
+
+
+
