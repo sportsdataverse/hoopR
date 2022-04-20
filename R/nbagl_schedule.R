817c1af643ac955e4edfd183d137f8103b3663c3
saiemgilani <saiem.gilani@gmail.com>
2022-04-20 12:13:31 -0400

    Update g-league functions and docs

---
+++ R/nbagl_schedule.R
@@ -0,0 +1,86 @@
+#' **Get G League Schedule from NBA API**
+#' @name nbagl_season_schedule
+NULL
+#' @title
+#' **Get G League Schedule from NBA API**
+#' @description Scrapes the NBA Data API for G League Schedule for a Given Season
+#' @rdname nbagl_season_schedule
+#' @author Billy Fryer
+#' @param season Season - 4 digit, i.e. 2021
+#' @return Returns a data frame of the G League Season Schedule
+#' @importFrom glue glue
+#' @importFrom jsonlite fromJSON
+#' @importFrom dplyr pull bind_rows arrange
+#' @import rvest
+#' @export
+
+nbagl_season_schedule <- function(season = most_recent_nba_season()-1) {
+  # From This Line to My next comment, basically everything is
+  # Copied from hoopR except the url and the table name
+  full_url <- glue::glue("https://data.nba.com/data/10s/v2015/json/mobile_teams/dleague/{season}/league/20_full_schedule.json")
+  tryCatch(
+    expr={
+      res <- httr::RETRY("GET", full_url)
+
+      resp <- res$content %>%
+        rawToChar() %>%
+        jsonlite::fromJSON(simplifyVector = T)
+
+      schedule_df <- resp$lscd$mscd %>%
+        jsonlite::toJSON() %>%
+        jsonlite::fromJSON(flatten=TRUE) %>%
+        tidyr::unnest(.data$g) %>%
+        janitor::clean_names() %>%
+        dplyr::arrange(.data$gdte) %>%
+        make_hoopR_data("NBA G-League Season Schedule Information from NBA.com",Sys.time())
+    },
+    error = function(e) {
+      message(glue::glue("{Sys.time()}: Invalid arguments or no season schedule data for {season} available!"))
+    },
+    warning = function(w) {
+    },
+    finally = {
+    }
+  )
+
+  return(schedule_df)
+}
+
+# Example JSON Link:
+# https://data.nba.com/data/10s/v2015/json/mobile_teams/dleague/2021/league/20_full_schedule.json
+
+# 2015 and Older does not work
+
+### Variable Explanation
+
+# gid = Game Id
+# gcode = GameDate/VisitorAbbreveationHomeAbbreviation
+# seri = Always returns ""
+# is = Always returns 1
+# gdte = Game Date
+# htm = Game Date and time for Home Team
+# vtm = Game Date and time for Visiting Team
+# etm = Game Date and time in Eastern
+# an = Arena where game was played
+# ac = City where game was played
+# as = Atate where game was played
+# st = Always "1"
+# stt = Game Start time in eastern for "12:00 pm EST"
+# bd.b = Data Frame with 6 variables about television broadcast
+# v.tid = Visitor Team Id
+# v.re = Visitor Record (?)
+# v.ta = Visitor Abbreviation
+# v.tn = Visitor Team Nickname
+# v.tc = Visitor Team City
+# v.s = NULL
+# h.tid = Home Team Id
+# h.re =  Home Record (?)
+# h.ta =  Home Abbreviation
+# h.tn =  Home Team Nickname
+# h.tc =  Home Team City
+# h.s = NULL
+# gdctm = Game Date but somehow different than gdte
+# utctm = ???
+# ppdst = ???
+# seq = Game Order By Day (1st game on 1-1-2022, 2nd game on 1-1-2022, 1st game on 1-2-2022, etc.)
+
---
+++ R/nbagl_standings.R
@@ -0,0 +1,71 @@
+#' **Get Current G League Standings from NBA API**
+#' @name nbagl_standings
+NULL
+#' @title
+#' **Get Current G League Standings from NBA API**
+#' @description Scrapes the NBA Data API for G League Standings
+#' @rdname nbagl_standings
+#' @author Billy Fryer
+#' @param season Season - 4 digit, i.e. 2021
+#' @return Returns a tibble of the G League Season Standings
+#' @importFrom glue glue
+#' @importFrom jsonlite fromJSON
+#' @importFrom dplyr pull bind_rows arrange
+#' @import rvest
+#' @export
+
+nbagl_standings <- function(season = most_recent_nba_season()-1) {
+
+  full_url <- glue::glue("https://s.data.nba.com/data/10s/v2015/json/mobile_teams/dleague/{season}/20_standings_05.json")
+  tryCatch(
+    expr={
+      res <- httr::RETRY("GET", full_url)
+
+      resp <- res$content %>%
+        rawToChar() %>%
+        jsonlite::fromJSON(simplifyVector = T)
+      # Find Table with the pbp in it
+      data <- resp$sta$co %>%
+        tidyr::unnest(.data$di, names_sep = '_') %>%
+        tidyr::unnest(.data$di_t) %>%
+        arrange(.data$see) %>%
+        make_hoopR_data("NBA G-League Standings Information from NBA.com",Sys.time())
+    },
+    error = function(e) {
+      message(glue::glue("{Sys.time()}: Invalid arguments or no G-League standings data for {season} available!"))
+    },
+    warning = function(w) {
+    },
+    finally = {
+    }
+  )
+
+
+  return(data)
+}
+# Example PBP JSON Link:
+# https://s.data.nba.com/data/10s/v2015/json/mobile_teams/dleague/2021/20_standings_05.json
+
+### Variable Explanation
+
+# tid = Team ID
+# see = Ranking
+# cli = ???
+# clid = ???
+# clic = ???
+# elim = ???
+# str = Win Streak. All data comes in format "W/L ##"
+# l10 = Last 10 Games Record
+# dr = ???
+# cr = ???
+# l = Losses
+# w = Wins
+# hr = Home Record
+# ar = Away Record
+# gb = Games Behind
+# gbd = Games Behind Division
+# crnk = Conference Rank? This appears to be the same as see
+# drnk = Division Rank
+# ta = Team Abbreviation
+# tn = Team Nickname
+# tc = Team City
