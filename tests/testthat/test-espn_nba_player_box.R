72306e279bfe15b88fa353c553bea6fcb0c6da44
saiemgilani <saiem.gilani@gmail.com>
2022-05-24 09:20:12 -0400

    add identifying columns to pbp for mbb and nba

--- tests/testthat/test-espn_nba_player_box.R
+++ tests/testthat/test-espn_nba_player_box.R
@@ -3,13 +3,38 @@ test_that("ESPN - Get NBA player box score only", {
   x <- espn_nba_player_box(game_id = 401283399)
 
   cols <- c(
-    "athlete_display_name", "team_short_display_name",
-    "min", "fg", "fg3", "ft", "oreb", "dreb", "reb", "ast", "stl", "blk",
-    "to", "pf", "+/-", "pts", "starter", "ejected", "did_not_play", "active",
-    "athlete_jersey", "athlete_id", "athlete_short_name",
-    'athlete_headshot_href',"athlete_position_name",
-    "athlete_position_abbreviation", "team_name", "team_logo",
-    "team_id", "team_abbreviation", "team_color", "team_alternate_color"
+    "athlete_display_name",
+    "team_short_display_name",
+    "min",
+    "fg",
+    "fg3",
+    "ft",
+    "oreb",
+    "dreb",
+    "reb",
+    "ast",
+    "stl",
+    "blk",
+    "to",
+    "pf",
+    "+/-",
+    "pts",
+    "starter",
+    "ejected",
+    "did_not_play",
+    "active",
+    "athlete_jersey",
+    "athlete_id",
+    "athlete_short_name",
+    "athlete_headshot_href",
+    "athlete_position_name",
+    "athlete_position_abbreviation",
+    "team_name",
+    "team_logo",
+    "team_id",
+    "team_abbreviation",
+    "team_color",
+    "team_alternate_color"
   )
   expect_equal(colnames(x), cols)
   expect_s3_class(x, 'data.frame')
